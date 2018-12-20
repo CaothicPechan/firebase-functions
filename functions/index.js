162d33569cb8d36881f15bbd5f5d7db18887a478
Rafael Ulises Mendoza Reyes <rumendoza@localhost.localdomain>
2018-12-20 15:44:02 -0600

    functions init

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+
+const language = require('@google-cloud/language');
+const client = new language.LanguageServiceClient();
+
 
 
 // // Create and Deploy Your First Cloud Functions
@@ -9,7 +15,31 @@ const admin = require('firebase-admin');
 //  response.send("Hello from Firebase!");
 // });
 admin.initializeApp();
+const app = express();
+app.use(cors({ origin: true }));
 
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const authenticate = async (req, res, next) => {
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+    res.status(403).send('Unauthorized');
+    return;
+  }
+  const idToken = req.headers.authorization.split('Bearer ')[1];
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedIdToken;
+    next();
+    return;
+  } catch(e) {
+    res.status(403).send('Unauthorized');
+    return;
+  }
+};
+app.use(authenticate);
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
@@ -22,3 +52,28 @@ exports.addMessage = functions.https.onRequest((req, res) => {
       return res.redirect(303, snapshot.ref.toString());
     });
 });
+
+
+// build multiple CRUD interfaces:
+// app.get('users/:id', (req, res) => {
+//     return admin.database().ref('/users').then((snapshot) => { return res.json(snapshot); })
+// });
+app.post('/users', (req, res) => {
+    const original = req.query.text;
+
+    return admin.database().ref('/users').push({original: original}).then((snapshot) => {
+        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+      return res.redirect(303, snapshot.ref.toString());
+    });
+});
+// app.put('/users/:id', (req, res) => {
+
+// });
+// app.delete('/users/:id', (req, res) => {
+
+// });
+app.get('/users', (req, res) => {
+    return admin.database().ref('/users').then((snapshot) => { return res.json(snapshot); })
+});
+
+exports.api = functions.https.onRequest(app);
